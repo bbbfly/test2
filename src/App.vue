d8a2a4b3425c0c1b9055094810a524bfb1969bae
bbbfly <907312279@qq.com>
2018-06-26 17:07:19 +0800

    init test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <img class="test-img" src="./assets/logo.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less" scoped >
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  .test-img{
+    width: 190px;
+    height: 180px;
+  }
+}
+</style>
